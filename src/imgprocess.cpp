3f89228391371c88577e36adce2f8ccb1e07b8fe
ZhijuCen <samzikeoi@gmail.com>
2024-10-07 23:17:32 +0800

    More colored keypoints to have distinct Left/Right body part.

--- src/imgprocess.cpp
+++ src/imgprocess.cpp
@@ -13,11 +13,37 @@ void drawBBox(cv::Mat &img, cv::Rect &bbox)
 
 void drawKeypoints(cv::Mat &img, Pose &pose)
 {
-    cv::Scalar kpColor{23., 192., 192.};
+    cv::Scalar kpColor{23., 192., 192.}, kpColorLeft{192., 46., 46.}, kpColorRight{46., 46., 192.};
     cv::Scalar lineColor{168., 168., 64.};
-    for (auto kp : pose.keypoints)
+    int radius = 5;
+    int thicknessInvisible = 1;
+    for (int i = 0; i < pose.keypoints.size(); i++)
     {
-        cv::circle(img, cv::Point2f(kp[0], kp[1]), 3, kpColor, cv::LineTypes::FILLED);
+        float x, y, visibility;
+        x = pose.keypoints.at(i).at(0);
+        y = pose.keypoints.at(i).at(1);
+        visibility = pose.keypoints.at(i).at(2);
+        if (i % 2 == 1 && i > 0)
+        {
+            if (visibility >= 0.5)
+                cv::circle(img, cv::Point2f(x, y), radius, kpColorLeft, cv::LineTypes::FILLED);
+            else
+                cv::circle(img, cv::Point2f(x, y), radius, kpColorLeft, thicknessInvisible, cv::LineTypes::LINE_4);
+        }
+        else if (i % 2 == 0 && i > 0)
+        {
+            if (visibility >= 0.5)
+                cv::circle(img, cv::Point2f(x, y), radius, kpColorRight, cv::LineTypes::FILLED);
+            else
+                cv::circle(img, cv::Point2f(x, y), radius, kpColorRight, thicknessInvisible, cv::LineTypes::LINE_4);
+        }
+        else
+        {
+            if (visibility >= 0.5)
+                cv::circle(img, cv::Point2f(x, y), radius, kpColor, cv::LineTypes::FILLED);
+            else
+                cv::circle(img, cv::Point2f(x, y), radius, kpColor, thicknessInvisible, cv::LineTypes::LINE_4);
+        }
     }
     for (auto conn : pose.keypointConns)
     {
